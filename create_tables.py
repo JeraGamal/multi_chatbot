c54816fd95aff1b4f0ea180ffcf7e45c98d3f1c4
JeraGamal <132104475+JeraGamal@users.noreply.github.com>
2024-11-19 14:56:22 +0200

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -1,10 +1,11 @@
 from app.database import Base, engine
 from app.models import User  # Ensure you import all models here
 
-# Create tables
-print("Creating database tables...")
-Base.metadata.create_all(bind=engine)
-print("Tables created successfully.")
+
+def create_tables():
+    print("Creating tables...")
+    Base.metadata.create_all(bind=engine)
+    print("Tables created.")
 
 
 if __name__ == "__main__":
