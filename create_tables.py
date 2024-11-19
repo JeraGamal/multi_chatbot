5e944c29371948e005a41ce153279a370b14d7d6
JeraGamal <132104475+JeraGamal@users.noreply.github.com>
2024-11-19 14:18:00 +0200

    Create create_tables.py

---
+++ create_tables.py
@@ -0,0 +1,7 @@
+from app.database import Base, engine
+from app.models import User  # Ensure you import all models here
+
+# Create tables
+print("Creating database tables...")
+Base.metadata.create_all(bind=engine)
+print("Tables created successfully.")
